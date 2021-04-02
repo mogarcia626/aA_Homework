9b6e72cc81b9e7bc86e9c3885bcf31819ab0b326
moustafa <mogarcia626@gmail.com>
2021-04-02 11:55:35 -0400

    Added Map class

--- W3D5/DIY ADTs.rb
+++ W3D5/DIY ADTs.rb
@@ -41,3 +41,37 @@ class Queue
     end
 
 end
+
+class Map
+    attr_reader :list
+
+    def initialize
+        @list = []
+    end
+
+    def set(key,value)
+        keys = @list.map { |ele| ele[0] }
+        if @list.empty? || !keys.include?(key)
+            @list << [key, value]
+        else
+            idx = keys.index(key)
+            @list[idx][1] = value       
+        end
+    end
+
+    def get(key)
+       @list.select { |sub| sub[0] == key }[0][1]
+    end
+
+    def delete(key)
+        @list.reject! { |sub| sub[0] == key }
+    end
+
+    def show
+        p @list
+    end
+
+
+end
+
+
