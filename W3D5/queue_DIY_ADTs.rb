a951e88af258bd1cd98f97cf0eaeba19cbb5fd40
moustafa <mogarcia626@gmail.com>
2021-04-02 12:25:26 -0400

    Organized folder structure

---
+++ W3D5/queue_DIY_ADTs.rb
@@ -0,0 +1,20 @@
+class Queue
+    attr_reader :queue
+
+    def initialize
+        @queue = []
+    end
+
+    def enqueue(el)
+        @queue.push(el)
+    end
+
+    def dequeue
+        @queue.shift
+    end
+    
+    def peek
+        @queue.first
+    end
+
+end
