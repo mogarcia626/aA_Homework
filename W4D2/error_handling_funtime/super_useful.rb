82809868716a4848dbcbcbe69c0200f8696d5acb
moustafa <mogarcia626@gmail.com>
2021-04-06 22:52:58 -0400

    Finished stupid monster project

---
+++ W4D2/error_handling_funtime/super_useful.rb
@@ -0,0 +1,64 @@
+# PHASE 2
+def convert_to_int(str)
+  Integer(str)
+end
+
+# PHASE 3
+FRUITS = ["apple", "banana", "orange"]
+
+class NotAFruitError < StandardError
+  def message
+    "Dawg, i said fruit.... wtf is this?"
+  end
+end
+
+class CoffeeError < StandardError
+  def message
+    "You got anything stronger?"
+  end
+end
+
+def reaction(maybe_fruit)
+  if FRUITS.include? maybe_fruit
+    puts "OMG, thanks so much for the #{maybe_fruit}!"
+  elsif maybe_fruit == 'coffee' 
+    raise CoffeeError 
+  else
+    raise NotAFruitError
+  end 
+end
+
+def feed_me_a_fruit
+  puts "Hello, I am a friendly monster. :)"
+
+  puts "Feed me a fruit! (Enter the name of a fruit:)"
+  maybe_fruit = gets.chomp
+  reaction(maybe_fruit) 
+end  
+
+# PHASE 4
+class BestFriend
+  def initialize(name, yrs_known, fav_pastime)
+    raise ArgumentError.new("name cannot be blank") if name.empty?
+    raise ArgumentError.new("No new friends") if yrs_known.to_i < 5
+    raise ArgumentError.new("favorite pasttime cannot be blank") if fav_pastime.empty?
+
+    @name = name
+    @yrs_known = yrs_known
+    @fav_pastime = fav_pastime
+  end
+
+  def talk_about_friendship
+    puts "Wowza, we've been friends for #{@yrs_known}. Let's be friends for another #{1000 * @yrs_known}."
+  end
+
+  def do_friendstuff
+    puts "Hey bestie, let's go #{@fav_pastime}. Wait, why don't you choose. 😄"
+  end
+
+  def give_friendship_bracelet
+    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me." 
+  end
+end
+
+
---
+++ W4D2/error_handling_funtime/user_script.rb
@@ -0,0 +1,11 @@
+require_relative 'super_useful'
+
+# puts "'five' == #{convert_to_int('five')}"
+
+feed_me_a_fruit
+
+blue = BestFriend.new('Tron', 5, 'Smoke a fat Dub')
+
+blue.talk_about_friendship
+blue.do_friendstuff
+blue.give_friendship_bracelet
